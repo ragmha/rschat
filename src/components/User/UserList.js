7a9b3811b35d40809716d64d3f9239dee794c252
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-10-26 13:40:13 +0300

    Wrap UserList with Link component from RRD 🔨

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import { Image, List } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import './index.css';
 const img = 'https://robohash.org/meow';
 
 class UserList extends Component {
-  handleUser = e => {};
-
   render() {
     const { user, index } = this.props;
     return (
-      <div className="userList">
-        <List animated verticalAlign="middle" size="big" key={index}>
-          <List.Item onClick={this.handleUser}>
-            <Image avatar src={img} />
-            <List.Content>
-              <List.Header>{user}</List.Header>
-            </List.Content>
-          </List.Item>
-        </List>
-      </div>
+      <Link to="/userProfile">
+        <div className="userList">
+          <List animated verticalAlign="middle" size="big" key={index}>
+            <List.Item>
+              <Image avatar src={img} />
+              <List.Content>
+                <List.Header>{user}</List.Header>
+              </List.Content>
+            </List.Item>
+          </List>
+        </div>
+      </Link>
     );
   }
 }
