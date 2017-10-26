5ca7555f824c48a389c38fdd4b32bbc2ad1248e8
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-10-26 13:41:17 +0300

    use RoomMenu component 🤕

--- src/components/Room/index.js
+++ src/components/Room/index.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Menu, Grid } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
+import RoomMenu from './RoomMenu';
 import UserList from '../User/UserList';
 import Message from '../Message';
 
-const logo =
-  'https://cdn.codemate.com/wp-content/uploads/2016/02/C-icon-codemate.png';
-
 class Room extends Component {
-  state = { activeItem: 'home' };
-
-  renderMenu() {
-    const { activeItem } = this.state;
-    const { logout } = this.props;
-
-    return (
-      <Menu pointing secondary>
-        <Menu.Item>
-          <img src={logo} alt="app-logo" />
-        </Menu.Item>
-        <Menu.Item name="home" active={activeItem === 'home'} />
-        <Menu.Menu position="right">
-          <Menu.Item
-            name="logout"
-            active={activeItem === 'logout'}
-            onClick={logout}
-          />
-        </Menu.Menu>
-      </Menu>
-    );
-  }
-
   renderUsers() {
     const { users } = this.props;
     return Object.keys(users).map((user, i) => (
@@ -42,7 +17,7 @@ class Room extends Component {
   render() {
     return (
       <div>
-        <div>{this.renderMenu()}</div>
+        <RoomMenu {...this.props} />
         <Grid>
           <Grid.Column width={5}>{this.renderUsers()}</Grid.Column>
           <Grid.Column width={9}>
