4bd26057e9d0e23dd8572856de957d76b882e69f
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-10-26 13:40:48 +0300

    add a RoomMenu component ✨

---
+++ src/components/Room/RoomMenu.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Menu } from 'semantic-ui-react';
+
+const logo =
+  'https://cdn.codemate.com/wp-content/uploads/2016/02/C-icon-codemate.png';
+
+class RoomMenu extends Component {
+  state = { activeItem: 'home' };
+
+  render() {
+    const { activeItem } = this.state;
+    const { logout } = this.props;
+
+    return (
+      <Menu pointing secondary>
+        <Menu.Item>
+          <img src={logo} alt="app-logo" />
+        </Menu.Item>
+        <Menu.Item name="home" active={activeItem === 'home'} />
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="logout"
+            active={activeItem === 'logout'}
+            onClick={logout}
+          />
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+export default connect()(RoomMenu);
