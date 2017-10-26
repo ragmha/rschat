8286d770093515fb588e499bb7fda9e8c8468b06
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-10-26 13:49:27 +0300

    add react-router-dom to Root component 🔥

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import UserProfile from '../components/UserProfile';
 import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/userProfile" component={UserProfile} />
+        </Switch>
+      </div>
+    </Router>
   </Provider>
 );
 
